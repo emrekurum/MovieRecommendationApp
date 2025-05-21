a070eb188b519558f740103b3abf1b5057bb9d22
Emre KURUM <emrekurum07@hotmail.com>
2025-05-21 19:00:36 +0300

    Updated

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 // App.tsx
+import 'react-native-gesture-handler';
 import React, { useState, useEffect }
 from 'react';
+
 import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from './src/navigation/AuthNavigator';
 import MainAppNavigator from './src/navigation/MainAppNavigator';
